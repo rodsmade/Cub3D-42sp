ed551760d7117ce9afad65bbfc48d7134d1cbbef
rodsmade <rodrigogregoldo@gmail.com>
2022-09-21 10:55:14 -0300

    added validation for file extension and access

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,13 +6,14 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 11:52:42 by gusalves          #+#    #+#             */
-/*   Updated: 2022/09/21 10:19:37 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/09/21 10:32:20 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "libft.h"				// libft
 # include <stdio.h>				// printf(), perror()
 # include <errno.h>				// errno (macro)
 # include <string.h>			// strerror()
--- src/cub3D.c
+++ src/cub3D.c
@@ -6,28 +6,31 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 11:52:14 by gusalves          #+#    #+#             */
-/*   Updated: 2022/09/21 10:18:04 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/09/21 10:51:36 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-char *err_nbr_to_string(int err_nbr)
+char *errcode_to_string(int errcode)
 {
 	char *strings[] = {
-		"",
-		"Wrong number of arguments. Run: " BWHITE "./cub3d " RESET \
-		UPURPLE "PATH_TO_MAP" RESET
+		BGRAY "Undefined error message" RESET,
+		"Run: " BWHITE "./cub3d " RESET UPURPLE "PATH_TO_MAP" RESET,
+		"File extension must be " UPURPLE ".cub" RESET
 	};
-	return (strings[err_nbr]);
+	return (strings[errcode]);
 }
 
-void	printerr_exit(int err_nbr)
+void	print_err_exit(int errcode)
 {
-	printf(PURPLEB " ❌ Error \n" RESET);
-	printf("%s\n", err_nbr_to_string(err_nbr));
+	printf(PURPLEB " ❌ Error " RESET "\n");
+	if (!errno)
+		printf("%s\n", errcode_to_string(errcode));
+	else
+		perror(NULL);
 	// cleanup();
-	exit(err_nbr);
+	exit(errcode || errno);
 }
 
 int	main(int argc, char **argv)
@@ -35,15 +38,12 @@ int	main(int argc, char **argv)
 	int	fd;
 
 	if (argc != 2)
-		printerr_exit(1);
-	fd = open("exoterico.txt", O_RDONLY);
+		print_err_exit(1);
+	if (ft_strncmp(&argv[1][ft_strlen(argv[1]) - 4], ".cub", 4) != 0)
+		print_err_exit(2);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-	{
-		perror(NULL);
-		printf("errno: %i\n", errno);
-		printf("%s\n", strerror(errno));
-		exit(errno);
-	}
+		print_err_exit(0);
 	printf("%s\n", argv[1]);
 	return (0);
 }
