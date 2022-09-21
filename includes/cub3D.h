c49532ea44599d0581322996e237faa135b02ef5
rodsmade <rodrigogregoldo@gmail.com>
2022-09-21 20:00:02 -0300

    adding sections to header for organisation purposes

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,15 +6,17 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 11:52:42 by gusalves          #+#    #+#             */
-/*   Updated: 2022/09/21 10:32:20 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/09/21 19:47:35 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+// ------------------------------------------------		INCLUDES	-----------
+
 # include "libft.h"				// libft
-# include "mlx.h"               // minilibx
+# include "mlx.h"				// minilibx
 # include <stdio.h>				// printf(), perror()
 # include <errno.h>				// errno (macro)
 # include <string.h>			// strerror()
@@ -24,7 +26,9 @@
 # include <stdlib.h> 			// exit()
 # include "ansi_color_codes.h"	// colours 🌈
 
-# define KEY_ESC	65307       // ESC keycode
+# define KEY_ESC	65307		// ESC keycode
+
+// ------------------------------------------------		STRUCTS		-----------
 
 typedef struct s_mlx_struct
 {
@@ -32,13 +36,26 @@ typedef struct s_mlx_struct
 	void	*window;
 }				t_mlx_struct;
 
-// 									window.c
+// ------------------------------------------------		ENUMS		-----------
+
+enum e_http_method {
+	SYSCALL_ERR,
+	WRONG_ARG_NO,
+	FILE_EXT_ERR
+};
+
+
+// ------------------------------------------------		PROTOTYPES	-----------
+// error_handling.c
+void	print_err_exit(int errcode);
+
+// window.c
 void	window(t_mlx_struct *mlx);
 
-// 									destroy.c
+// destroy.c
 int		destroy(t_mlx_struct *mlx);
 
-// 									key_press.c
+// key_press.c
 int		keystrokes_management(int keycode, t_mlx_struct *mlx);
 
 #endif
