c5c56978dcd23a5af4d8793a3b4e70bb3be0ac4c
gstv <gustavdlima@gmail.com>
2022-09-22 10:56:01 -0300

    new logic to avoid img bad render

--- src/window/window.c
+++ src/window/window.c
@@ -18,16 +18,24 @@ static void	image(t_mlx_struct *mlx)
 	int y;
 
 	mlx->img = malloc(sizeof(t_mlx_img));
-	x = 150;
-	y = 150;
+	x = 0;
+	y = 0;
 	mlx->img->pointer = mlx_xpm_file_to_image
 		(mlx->pointer, IMG_TEST, &x, &y);
-	mlx_put_image_to_window(mlx->pointer, mlx->window, mlx->img->pointer, 250, 250);
+	while (x < 150) 
+	{
+		while (y < 150)
+		{
+			mlx_put_image_to_window(mlx->pointer, mlx->window, mlx->img->pointer, x, y);
+			y++;
+		}
+		x++;
+	}
 }
 
 void	window(t_mlx_struct *mlx)
 {
-	mlx->window = mlx_new_window(mlx->pointer, 500, 500, "cub3D");
+	mlx->window = mlx_new_window(mlx->pointer, 150, 150, "cub3D");
 	image(mlx);
 	mlx_hook(mlx->window, 17, 0L, destroy, mlx);
 	mlx_hook(mlx->window, 3, 1L << 1, keystrokes_management, mlx);
