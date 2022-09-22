3ca4b1f5e963c5f8762e401c8943310bece39a67
gstv <gustavdlima@gmail.com>
2022-09-22 20:58:33 -0300

    resolve segmentation fault

--- src/destroy/destroy.c
+++ src/destroy/destroy.c
@@ -17,9 +17,9 @@ int	destroy(t_mlx_struct *mlx)
 	printf("Closing the game...\n");
 	mlx_loop_end(mlx->pointer);
 	mlx_clear_window(mlx->pointer, mlx->window);
+	mlx_destroy_image(mlx->pointer, mlx->img->pointer);
 	mlx_destroy_window(mlx->pointer, mlx->window);
 	mlx_destroy_display(mlx->pointer);
-	mlx_destroy_image(mlx->pointer, mlx->img->pointer);
 	free(mlx->pointer);
 	exit (42);
 }
