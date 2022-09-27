b8c4dd6f83187a8c1d2b5ff64df18f76e07a78ef
rodsmade <rodrigogregoldo@gmail.com>
2022-09-26 21:55:36 -0300

    added tests for floor colour parameters validation

--- tests/map_validator.sh
+++ tests/map_validator.sh
@@ -43,7 +43,6 @@ LOG_FILE='test_00_err'
 
 # ASSERT
 ../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
-# ../cub3D $INPUT_FILE
 if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
 	printf "$BGRN[OK]$RESET 00_test_texture_params_format_ID_space_VALID_PATH_must_return_exit_code_0\n"
 else
@@ -59,7 +58,6 @@ LOG_FILE='test_01_err'
 
 # ASSERT
 ../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
-# ../cub3D $INPUT_FILE
 if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
 	printf "$BGRN[OK]$RESET 01_test_texture_params_format_ID_many_spaces_VALID_PATH_must_return_exit_code_0\n"
 else
@@ -75,11 +73,145 @@ LOG_FILE='test_02_err'
 
 # ASSERT
 ../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
-# ../cub3D $INPUT_FILE
 if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
 	printf "$BGRN[OK]$RESET 02_test_NO_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1\n"
 else
 	printf "$BRED[NOK]$RESET 02_test_NO_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1\n"
+fi
+}
+
+03_test_SO_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1() {
+# ARRANGE
+INPUT_FILE='./valid_maps/03_invalid_SO_texture_parameter.cub'
+EXPECTED_EXIT_CODE=1
+LOG_FILE='test_03_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 03_test_SO_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1\n"
+else
+	printf "$BRED[NOK]$RESET 03_test_SO_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1\n"
+fi
+}
+
+04_test_EA_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1() {
+# ARRANGE
+INPUT_FILE='./valid_maps/04_invalid_EA_texture_parameter.cub'
+EXPECTED_EXIT_CODE=1
+LOG_FILE='test_04_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 04_test_EA_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1\n"
+else
+	printf "$BRED[NOK]$RESET 04_test_EA_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1\n"
+fi
+}
+
+05_test_WE_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1() {
+# ARRANGE
+INPUT_FILE='./valid_maps/05_invalid_WE_texture_parameter.cub'
+EXPECTED_EXIT_CODE=1
+LOG_FILE='test_05_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 05_test_WE_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1\n"
+else
+	printf "$BRED[NOK]$RESET 05_test_WE_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1\n"
+fi
+}
+
+06_test_floor_colour_format_F_space_255_comma_255_comma_255_must_return_exit_code_0() {
+# ARRANGE
+INPUT_FILE='./valid_maps/06_valid_colour_parameters.cub'
+EXPECTED_EXIT_CODE=0
+LOG_FILE='test_06_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 06_test_floor_colour_format_F_space_255_comma_255_comma_255_must_return_exit_code_0\n"
+else
+	printf "$BRED[NOK]$RESET 06_test_floor_colour_format_F_space_255_comma_255_comma_255_must_return_exit_code_0\n"
+fi
+}
+
+07_test_floor_colour_format_F_many_spaces_255_space_comma_255_space_comma_255_must_return_exit_code_0() {
+# ARRANGE
+INPUT_FILE='./valid_maps/07_floor_colour_elements_separated_by_many_spaces.cub'
+EXPECTED_EXIT_CODE=0
+LOG_FILE='test_07_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 07_test_floor_colour_format_F_many_spaces_255_space_comma_255_space_comma_255_must_return_exit_code_0\n"
+else
+	printf "$BRED[NOK]$RESET 07_test_floor_colour_format_F_many_spaces_255_space_comma_255_space_comma_255_must_return_exit_code_0\n"
+fi
+}
+
+08_test_floor_colour_format_F_space_255_comma_255_comma_255_comma_must_return_exit_code_1() {
+# ARRANGE
+INPUT_FILE='./valid_maps/08_floor_colour_elements_with_trailing_commas.cub'
+EXPECTED_EXIT_CODE=1
+LOG_FILE='test_08_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 08_test_floor_colour_format_F_space_255_comma_255_comma_255_comma_must_return_exit_code_1\n"
+else
+	printf "$BRED[NOK]$RESET 08_test_floor_colour_format_F_space_255_comma_255_comma_255_comma_must_return_exit_code_1\n"
+fi
+}
+
+09_test_floor_colour_format_F_space_255_comma_255_comma_256_must_return_exit_code_1() {
+# ARRANGE
+INPUT_FILE='./valid_maps/09_floor_colour_elements_value_above_range.cub'
+EXPECTED_EXIT_CODE=1
+LOG_FILE='test_09_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 09_test_floor_colour_format_F_space_255_comma_255_comma_256_must_return_exit_code_1\n"
+else
+	printf "$BRED[NOK]$RESET 09_test_floor_colour_format_F_space_255_comma_255_comma_256_must_return_exit_code_1\n"
+fi
+}
+
+10_test_floor_colour_format_F_space_255_comma_255_comma_negative_1_must_return_exit_code_1() {
+# ARRANGE
+INPUT_FILE='./valid_maps/10_floor_colour_elements_value_bellow_range.cub'
+EXPECTED_EXIT_CODE=1
+LOG_FILE='test_10_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 10_test_floor_colour_format_F_space_255_comma_255_comma_negative_1_must_return_exit_code_1\n"
+else
+	printf "$BRED[NOK]$RESET 10_test_floor_colour_format_F_space_255_comma_255_comma_negative_1_must_return_exit_code_1\n"
+fi
+}
+
+11_test_floor_colour_format_F_space_255_comma_255_comma_2SS_must_return_exit_code_1() {
+# ARRANGE
+INPUT_FILE='./valid_maps/11_floor_colour_elements_value_alfanumeric.cub'
+EXPECTED_EXIT_CODE=1
+LOG_FILE='test_11_err'
+
+# ASSERT
+../cub3D $INPUT_FILE > $LOGS_PATH/$LOG_FILE 2>&1
+if [[ $? -eq EXPECTED_EXIT_CODE ]]; then
+	printf "$BGRN[OK]$RESET 11_test_floor_colour_format_F_space_255_comma_255_comma_2SS_must_return_exit_code_1\n"
+else
+	printf "$BRED[NOK]$RESET 11_test_floor_colour_format_F_space_255_comma_255_comma_2SS_must_return_exit_code_1\n"
 fi
 }
 
@@ -117,3 +249,12 @@ fi
 00_test_texture_params_format_ID_space_VALID_PATH_must_return_exit_code_0
 01_test_texture_params_format_ID_many_spaces_VALID_PATH_must_return_exit_code_0
 02_test_NO_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1
+03_test_SO_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1
+04_test_EA_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1
+05_test_WE_texture_param_format_ID_space_INVALID_PATH_must_return_exit_code_1
+06_test_floor_colour_format_F_space_255_comma_255_comma_255_must_return_exit_code_0
+07_test_floor_colour_format_F_many_spaces_255_space_comma_255_space_comma_255_must_return_exit_code_0
+08_test_floor_colour_format_F_space_255_comma_255_comma_255_comma_must_return_exit_code_1
+09_test_floor_colour_format_F_space_255_comma_255_comma_256_must_return_exit_code_1
+10_test_floor_colour_format_F_space_255_comma_255_comma_negative_1_must_return_exit_code_1
+11_test_floor_colour_format_F_space_255_comma_255_comma_2SS_must_return_exit_code_1
