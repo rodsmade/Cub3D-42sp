9661b6e751dd7ec2bc5678cd1e334212779baa62
rodsmade <rodrigogregoldo@gmail.com>
2022-12-01 16:19:11 -0300

    texture paths coming from parameters in input file

--- src/texture_loading.c
+++ src/texture_loading.c
@@ -6,20 +6,21 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 15:42:40 by gusalves          #+#    #+#             */
-/*   Updated: 2022/12/01 16:01:15 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/12/01 16:16:06 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-static void	load_image(t_data *data, int *texture, char *path)
+static void	load_texture(t_data *data, int direction)
 {
 	int			i;
 	int			j;
 	t_mlx_img	img;
 
 	i = 0;
-	img.pointer = mlx_xpm_file_to_image(data->mlx.pointer, path,
+	img.pointer = mlx_xpm_file_to_image(data->mlx.pointer,
+			data->map_data.texture_paths[direction],
 			&img.img_width, &img.img_height);
 	img.data = (int *)mlx_get_data_addr(img.pointer,
 			&img.bits_per_pixel, &img.line_lenght, &img.endian);
@@ -28,7 +29,7 @@ static void	load_image(t_data *data, int *texture, char *path)
 		j = 0;
 		while (j < img.img_width)
 		{
-			texture[img.img_width * i + j]
+			data->ray.texture[direction][img.img_width * i + j]
 				= img.data[img.img_width * i + j];
 			j++;
 		}
@@ -39,8 +40,8 @@ static void	load_image(t_data *data, int *texture, char *path)
 
 void	load_textures(t_data *data)
 {
-	load_image(data, data->ray.texture[NO], "./assets/textures/bluestone.xpm");
-	load_image(data, data->ray.texture[SO], "./assets/textures/redbrick.xpm");
-	load_image(data, data->ray.texture[EA], "./assets/textures/purplestone.xpm");
-	load_image(data, data->ray.texture[WE], "./assets/textures/greystone.xpm");
+	load_texture(data, NO);
+	load_texture(data, SO);
+	load_texture(data, EA);
+	load_texture(data, WE);
 }
