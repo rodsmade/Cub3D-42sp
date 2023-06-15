08819aa22232676120fb81662f0892bd35eb401a
rodsmade <rodrigogregoldo@gmail.com>
2023-06-14 22:16:03 -0300

    norminette fix

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 19:13:38 by roaraujo          #+#    #+#             */
-/*   Updated: 2023/06/14 13:16:34 by roaraujo         ###   ########.fr       */
+/*   Updated: 2023/06/14 22:15:44 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 void	set_hooks(t_data *data)
 {
 	mlx_hook(data->mlx.window, DestroyNotify, NoEventMask, destroy, data);
-	mlx_hook(data->mlx.window, KeyPress, KeyPressMask, keystrokes_management, data);
+	mlx_hook(data->mlx.window, KeyPress, KeyPressMask, \
+											keystrokes_management, data);
 	mlx_loop_hook(data->mlx.pointer, &raycasting, data);
 	return ;
 }
